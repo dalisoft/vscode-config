78645549f5e291572b53394ecf4e71f4d864060a
Davlatjon Sh <dalisoft.dev@gmail.com>
2025-07-20 19:23:20 +0500

    feat: merge No-AI and AI modes into single branch (#1)

--- install-cursor.sh
+++ install-cursor.sh
@@ -23,14 +23,14 @@ cp keybindings.json "$APPS_PATH/Cursor/User/keybindings.json"
 if [ -n "$(type cursor)" ]; then
   echo "Found Cursor, installing configs..."
 
-  INSTALLED_EXTENSIONS=$(code --list-extensions)
-  cat extensions.txt | while read -r ext; do
+  INSTALLED_EXTENSIONS=$(cursor --list-extensions)
+  while read -r ext; do
     if [ "$(echo "$INSTALLED_EXTENSIONS" | grep -o "$ext")" = "$ext" ]; then
       echo "Already exists extension: $ext"
     else
       cursor --install-extension "$ext" || echo "Failed to install: $ext"
     fi
-  done
+  done < "$PWD/extensions.txt"
 
   echo "Done for Cursor"
 fi
--- install-insider.sh
+++ install-insider.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -eu
 
 APPS_PATH=""
 INSTALLED_EXTENSIONS=""
@@ -24,13 +24,23 @@ if [ -n "$(type code-insiders)" ]; then
   echo "Found VSCode Insiders, installation..."
 
   INSTALLED_EXTENSIONS=$(code-insiders --list-extensions)
-  cat extensions.txt | while read -r ext; do
+  while read -r ext; do
     if [ "$(echo "$INSTALLED_EXTENSIONS" | grep -o "$ext")" = "$ext" ]; then
       echo "Already exists extension: $ext"
     else
       code-insiders --install-extension "$ext" || echo "Failed to install: $ext"
     fi
-  done
+  done < "$PWD/extensions.txt"
+
+  if [ "${1-}" = "--ai" ]; then
+    while read -r ext; do
+    if [ "$(echo "$INSTALLED_EXTENSIONS" | grep -o "$ext")" = "$ext" ]; then
+      echo "Already exists extension: $ext"
+    else
+      code-insiders --install-extension "$ext" || echo "Failed to install: $ext"
+    fi
+  done < "$PWD/ai.txt"
+  fi
 
   echo "Done for VSCode Insiders"
 fi
